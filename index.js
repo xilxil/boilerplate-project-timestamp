1f41ff2f798668594035b5119553f0987abfacd7
Onyrixx <djeumodimitri@gmail.com>
2024-06-07 13:32:12 +0100

    Initial commit

--- index.js
+++ index.js
@@ -24,7 +24,40 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get('/api', (req, res) => {
+  const currentDate = new Date();
+  res.json({
+    unix: currentDate.getTime(),
+    utc: currentDate.toUTCString(),
+  });
+});
 
+app.get('/api/:timestamp', (req, res) => {
+  const timestamp = req.params.timestamp;
+
+  // Check if the timestamp is a valid Unix time in milliseconds
+  if (!isNaN(timestamp)) {
+    const date = new Date(Number(timestamp));
+    if (date.toString() !== 'Invalid Date') {
+      return res.json({
+        unix: date.getTime(),
+        utc: date.toUTCString(),
+      });
+    }
+  }
+
+  // Check if the timestamp is a valid date string
+  const date = new Date(timestamp);
+  if (date.toString() !== 'Invalid Date') {
+    return res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString(),
+    });
+  }
+
+  // If the input is not valid
+  res.json({ error: 'Invalid Date' });
+});
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
